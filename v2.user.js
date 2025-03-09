0f4c7e8b995ba861459e2eca8393cdb954e8861c
Nopermc <nopermc02@gmail.com>
2025-03-09 19:49:36 +0700

    Create v2.user.js

---
+++ v2.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Yeumoney Bypasser by Nopermc
+// @namespace    http://tampermonkey.net/
+// @version      9.0
+// @description  The Ultimate Yeumoney Bypass BeTa  (Nopermc)
+// @author       NoperMc
+// @match        *://yeumoney.com/*
+// @match        *://docs.google.com/spreadsheets/*
+// @match        *://docs.google.com/forms/*
+// @grant        GM_xmlhttpRequest
+// @require      https://raw.githubusercontent.com/Nopermc/bypassyeumoney/refs/heads/main/Test1.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @icon         https://thecard1s.shop/assets/storage/images/image_OD4HV.png
+// ==/UserScript==
